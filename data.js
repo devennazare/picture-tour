b193b14955bdc24b2867d006969e5059b52846ff
Deven@12345 <deven@ottoedge.com>
2021-10-26 13:13:19 +0530

    first commit

---
+++ data.js
@@ -0,0 +1,87 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-timothy-oldfield-luufnhochru-unsplash",
+      "name": "timothy-oldfield-luufnHoChRU-unsplash",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.4126924692571101
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -0.19578894466847174,
+          "pitch": -0.32452906979634655,
+          "title": "Entry Point",
+          "text": "Welcome to the Mansion&nbsp;"
+        }
+      ]
+    },
+    {
+      "id": "1-screentwo",
+      "name": "screentwo",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 0.0007005859000823733,
+          "pitch": 0,
+          "title": "Welcome to Second Screen",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
